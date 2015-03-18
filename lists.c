abb9d12807e310be8e1308bee70aff8f93364615
thevks <vkumar.sharma@gmail.com>
2015-03-18 19:12:26 +0530

    added recursion

--- lists.c
+++ lists.c
@@ -36,6 +36,21 @@ void printList(node_t *root)
     printf("\n");
 }
 
+void printUsingRecursion(node_t *root)
+{
+    if (root) {
+        printf("%d ", root->item);
+        printUsingRecursion(root->next);
+    }
+}
+
+void printReverseUsingRecursion(node_t *root)
+{
+    if (root->next) 
+        printReverseUsingRecursion(root->next);
+    printf("%d ", root->item);
+}
+
 //Compares two lists
 int compare_lists(node_t *p, node_t *q)
 {
@@ -53,26 +68,52 @@ int compare_lists(node_t *p, node_t *q)
     return flag;
 }
 
+node_t *copy_list(node_t *root)
+{
+    node_t *temp = NULL;
+    if (root){
+        temp = (node_t *)malloc(sizeof(node_t)); 
+        assert(temp != NULL);
+        temp->item = root->item;
+        temp->next = copy_list(root->next);
+    }
+    return temp;
+}
+
 int main(void)
 {
-    node_t *head1 = NULL, *head2 = NULL;
+    node_t *head1 = NULL, *head2 = NULL, *head3 = NULL;
 
     addNode(&head1, 40);
     addNode(&head1, 20);
     addNode(&head1, 30);
     addNode(&head1, 10);
+    printf("Head 1 ---> ");
     printList(head1);
 
     addNode(&head2, 40);
     addNode(&head2, 20);
-    addNode(&head2, 30);
+    addNode(&head2, 50);
     addNode(&head2, 10);
+    printf("Head 2 ---> ");
     printList(head2);
 
     if (compare_lists(head1, head2))
         printf("Lists are equal\n");
     else
         printf("Lists are not equal\n");
+
+    head3 = copy_list(head2);
+    printf("Head 3 ---> ");
+    printList(head3);
+
+    printf("Head 3 ---> ");
+    printReverseUsingRecursion(head3);
+    printf("\n");
+
+    printf("Head 3 ---> ");
+    printUsingRecursion(head3);
+    printf("\n");
 
     return 0;
 }
