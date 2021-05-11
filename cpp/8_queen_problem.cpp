f74a9c5565aa7f17bc10889ed577aaaea3d27387
thevks <vkumar.sharma@gmail.com>
2021-05-11 10:20:20 -0700

    correction

--- cpp/8_queen_problem.cpp
+++ cpp/8_queen_problem.cpp
@@ -1,8 +1,12 @@
-//program to solve the n queen problem 
-//grid[][] is represent the 2-d array with value(0 and 1) for grid[i][j]=1 means queen i are placed at j column.
-//we can take any number of queen , for this time we take the atmost 10 queen (grid[10][10]).
+/* program to solve the n queen problem 
+ * grid[][] is represent the 2-d array with value(0 and 1) 
+ * for grid[i][j]=1 means queen i are placed at j column.
+ * we can take any number of queen, for this time we take 
+ * the atmost 10 queen (grid[10][10]).
+ */
 #include<iostream>
 using namespace std;
+
 int grid[10][10];
 
 //print the solution
@@ -45,40 +49,42 @@ bool isSafe(int col, int row, int n) {
 
 //function to find the position for each queen
 //row is indicates the queen no. and col represents the possible positions
-bool solve (int n, int row) {
+bool solve (int row, int n) {
+    
     if (n == row) {
         print(n);
         return true;
     }
+    
     //variable res is use for possible backtracking 
     bool res = false;
     for (int i = 0;i <=n-1;i++) {
         if (isSafe(i, row, n)) {
-            grid[row][i] = 1;
-            //recursive call solve(n, row+1) for next queen (row+1)
-            res = solve(n, row+1) || res;//if res ==false then backtracking will occur 
-            //by assigning the grid[row][i] = 0
             
+            grid[row][i] = 1;
+            res = solve(row+1, n) || res;
             grid[row][i] = 0;
+            
         }
     }
+    
     return res;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
     int n;
     cout<<"Enter the number of queen"<<endl;
     cin >> n;
+ 
     for (int i = 0;i < n;i++) {
         for (int j = 0;j < n;j++) {
             grid[i][j] = 0;
         }
     }
-    bool res = solve(n, 0);
+ 
+    bool res = solve(0, n);
+ 
     if(res == false) {
         cout << -1 << endl; //if there is no possible solution
     } else {
